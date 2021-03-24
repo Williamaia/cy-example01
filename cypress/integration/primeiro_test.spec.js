5c4118aa6fd5e817767dc8d3ce506643eeef48e6
William Maia <williamaiadias@hotmail.com>
2021-03-23 22:57:03 -0300

    Adicionado teste 'deve calcular o imposto devido' ao projeto

--- cypress/integration/primeiro_test.spec.js
+++ cypress/integration/primeiro_test.spec.js
@@ -1,11 +1,11 @@
 describe('Calculadora de imposto', () => {
     it('deve limpar os campos', () => {
         cy.visit('https://andreendo.github.io/sample-ui-compras/index-2.html');
-        cy.get('[name=nome]').type('John');
-        cy.get('[name=idade]').type('20');
-        cy.get('[name=sexo]').select('M');
-        cy.get('[name=salario]').type('2000');
-        // cy.get(`[value='Limpar campos']`).click();
+        cy.get('[name=nome]').type('William');
+        cy.get('[name=idade]').type('26');
+        cy.get('[name=sexo]').select('F');
+        cy.get('[name=salario]').type('3000');
+        cy.get(`[value='Limpar campos']`).click();
 
         cy.get('[name=nome]').should('have.text', '');
         cy.get('[name=idade]').should('have.text', '');
@@ -15,9 +15,27 @@ describe('Calculadora de imposto', () => {
 
     it('deve calcular o imposto devido', () => {
         // preencha todos os campos
-
+        cy.visit('https://andreendo.github.io/sample-ui-compras/index-2.html');
+        cy.get('[name=nome]').type('John');
+        cy.get('[name=idade]').type('20');
+        cy.get('[name=sexo]').select('M');
+        cy.get('[name=salario]').type('2000');
+       
         // clique no botao calcular
+        cy.get(`[value='Calcular']`).click();
 
         // verifique (usando .should) a saída retornada com o calculo do imposto devido
+        cy.get('[name=salario]')
+        .then(value => value.val())
+        .then( salario => {
+            let valor = "";
+            if(salario > 5000){
+                valor = salario * 0.25;
+            }else{
+                valor = salario * 0.15
+            }
+            
+            cy.get('[id=divValorImposto]').should('have.text', 'Valor a pagar de imposto: ' + valor);
+        });
     });
 });
